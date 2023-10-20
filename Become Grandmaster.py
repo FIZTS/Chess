3a8fef44e16d2556b6e1531971197b8648dc7d72
FIZTS <nonggear40@gmail.com>
2023-10-20 18:04:48 +0700

    publish

---
+++ Become Grandmaster.py
@@ -0,0 +1,29 @@
+'''
+8*8 mattrix
+7 types of pieces = 7 move patterns
+2 sides (black and white)
+import picture (chess.com or download)
+
+make a screen
+
+Turn
+    - White turn
+    - White Select
+        - White select new pieces
+            -White select possible move
+        - White select possible move
+    - Black turn
+        - Black select new pieces
+            -White select possible move
+        - White select possible move
+
+captured pieces are thrown to the side of the board became smaller
+remind check with highlighting
+
+game ended with check and no escape
+    - How about stalement??
+
+Restart game
+    - Should the board be flipped??
+'''
+
